b065d56f5870fe49ab61ba6b9c06cab728e826a0
Christopher Garcia <christopher.garcia08@gmail.com>
2021-04-23 05:17:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,25 +1,28 @@
 class Artist < Super
   
-  attr_accessor :songs 
+  attr_accessor :name 
+  
+  @@all = []
   
   def initialize(name)
     @name = name
-    @songs = []
+    @all << self
   end
   
-  def self.create(name)
-    instance = Artist.new(name)
-    instance.save
-    instance
+  def songs 
+    Song.all.select {|song| song.artist == self}
   end
   
   def genres 
-    self.songs.map {|song|song.genre}.uniq 
+    songs.map {|song|song.genre} 
   end
   
-  def add_song(song)
-    song.artist = self if song.artist == nil
-    self.songs << song if self.songs.include?(song) == false
+  def self.all
+    @@all
+  end
+  
+  def new_song(song, genre)
+    Song.new(name, self, genre)
   end
   
 end
