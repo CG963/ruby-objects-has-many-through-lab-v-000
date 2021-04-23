fdce59652b63de361b931f66d31841883bae7cea
Christopher Garcia <christopher.garcia08@gmail.com>
2021-04-23 05:21:14 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+class Doctor
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end  
+  
+  def appointments
+  Meal.all.select do |meal|
+    meal.waiter == self #checking for waiter now
+    end
+  end
+  
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+ 
+    best_tipped_meal.customer
+  end
+end
