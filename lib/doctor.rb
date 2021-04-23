d16250c7ef56eaf9a3d2676335cc837df1e6eefa
Christopher Garcia <christopher.garcia08@gmail.com>
2021-04-23 05:51:24 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,31 +1,26 @@
-class Doctor 
-  attr_accessor :name 
+class Doctor
+  attr_accessor :name
 
   @@all = []
 
   def initialize(name)
-    @name = name 
-    @@all << self 
-  end   
+    @name = name
+    @@all << self
+  end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
   def new_appointment(date, patient)
-    Appointment.new(date, self, patient)
-  end   
+    Appointment.new(date, patient, self)
+  end
 
-  def appointments 
-    Appointment.all.select do |appointment|
-      appointment.doctor == self 
-    end 
-  end   
+  def appointments
+    Appointment.all.select { |appointment| appointment.doctor == self }
+  end
 
-  def patients 
-    appointments.map do |appointment|
-      appointment.patient
-    end 
-  end   
-
+  def patients
+    appointments.map(&:patient)
+  end
 end
